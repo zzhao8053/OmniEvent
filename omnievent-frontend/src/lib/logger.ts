70e0a628b09bd12f76a0ad576d940bc4cd41b36b
dt_3414403343 <zzhao8053@gmail.com>
2026-05-09 10:15:24 +0800

    feat(frontend): 迁移 logger

--- omnievent-frontend/src/lib/logger.ts
+++ omnievent-frontend/src/lib/logger.ts
@@ -1,61 +1,42 @@
-const LOG_PREFIX = '[OmniEvent]'
+import { isEnableDebug } from './settings.ts';
 
-export enum LogLevel {
-  DEBUG = 0,
-  INFO = 1,
-  WARN = 2,
-  ERROR = 3
+function logDebug(msg: string, obj?: unknown): void {
+    if (isEnableDebug()) {
+        if (obj) {
+            console.debug('[ezBookkeeping Debug] ' + msg, obj);
+        } else {
+            console.debug('[ezBookkeeping Debug] ' + msg);
+        }
+    }
 }
 
-let currentLogLevel = LogLevel.INFO
-
-export function setLogLevel(level: LogLevel): void {
-  currentLogLevel = level
+function logInfo(msg: string, obj?: unknown): void {
+    if (obj) {
+        console.info('[ezBookkeeping Info] ' + msg, obj);
+    } else {
+        console.info('[ezBookkeeping Info] ' + msg);
+    }
 }
 
-export function isDebugEnabled(): boolean {
-  return currentLogLevel <= LogLevel.DEBUG
+function logWarn(msg: string, obj?: unknown): void {
+    if (obj) {
+        console.warn('[ezBookkeeping Warn] ' + msg, obj);
+    } else {
+        console.warn('[ezBookkeeping Warn] ' + msg);
+    }
 }
 
-function formatMessage(level: string, message: string, ...args: any[]): string {
-  const timestamp = new Date().toISOString()
-  const formattedArgs = args.length > 0 ? ' ' + args.map(arg =>
-    typeof arg === 'object' ? JSON.stringify(arg) : String(arg)
-  ).join(' ') : ''
-  return `${timestamp} ${LOG_PREFIX} [${level}] ${message}${formattedArgs}`
+function logError(msg: string, obj?: unknown): void {
+    if (obj) {
+        console.error('[ezBookkeeping Error] ' + msg, obj);
+    } else {
+        console.error('[ezBookkeeping Error] ' + msg);
+    }
 }
 
-export function debug(message: string, ...args: any[]): void {
-  if (currentLogLevel <= LogLevel.DEBUG) {
-    console.debug(formatMessage('DEBUG', message, ...args))
-  }
-}
-
-export function info(message: string, ...args: any[]): void {
-  if (currentLogLevel <= LogLevel.INFO) {
-    console.info(formatMessage('INFO', message, ...args))
-  }
-}
-
-export function warn(message: string, ...args: any[]): void {
-  if (currentLogLevel <= LogLevel.WARN) {
-    console.warn(formatMessage('WARN', message, ...args))
-  }
-}
-
-export function error(message: string, ...args: any[]): void {
-  if (currentLogLevel <= LogLevel.ERROR) {
-    console.error(formatMessage('ERROR', message, ...args))
-  }
-}
-
-export const logger = {
-  debug,
-  info,
-  warn,
-  error,
-  setLogLevel,
-  isDebugEnabled
-}
-
-export default logger
+export default {
+    debug: logDebug,
+    info: logInfo,
+    warn: logWarn,
+    error: logError
+};
